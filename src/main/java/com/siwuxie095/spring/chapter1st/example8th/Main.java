f4439893bdede222e8a4e4e34fe781907beab89d
siwuxie095 <834879583@qq.com>
2020-12-18 08:11:08 +0800

    俯瞰 Spring 风景线；

---
+++ src/main/java/com/siwuxie095/spring/chapter1st/example8th/Main.java
@@ -0,0 +1,22 @@
+package com.siwuxie095.spring.chapter1st.example8th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-18 08:06:37
+ */
+public class Main {
+
+    /**
+     * 俯瞰 Spring 风景线
+     *
+     * 正如你所看到的，Spring 框架关注于通过 DI、AOP 和消除样板式代码来简化企业级 Java 开发。即使这是 Spring
+     * 所能做的全部事情，那 Spring 也值得一用。但是，Spring 实际上的功能超乎你的想象。
+     *
+     * 在 Spring 框架的范畴内，你会发现 Spring 简化 Java 开发的多种方式。但在 Spring 框架之外还存在一个构建
+     * 在核心框架之上的庞大生态圈，它将 Spring 扩展到不同的领域，例如 Web 服务、REST、移动开发以及 NoSQL。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
