16e3236c45c49da8ad26668cdef8e2e87bcc4db2
siwuxie095 <834879583@qq.com>
2020-12-25 22:29:53 +0800

    高级装配；

---
+++ src/main/java/com/siwuxie095/spring/chapter3rd/example1st/Main.java
@@ -0,0 +1,22 @@
+package com.siwuxie095.spring.chapter3rd.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-25 22:26:02
+ */
+public class Main {
+
+    /**
+     * 高级装配
+     *
+     * 前面已经看到了 Spring 最为核心的 bean 装配技术，除此之外，Spring 还提供了多种技巧，
+     * 借助它们可以实现更为高级的 bean 装配功能。
+     *
+     * 这里将会深入介绍一些这样的高级技术。这里所介绍的技术也许你不会天天都用到，但这并不意味
+     * 着它们的价值会因此而降低。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
