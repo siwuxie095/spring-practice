4a77d3f51d1fdbdebde9b1d03293efc16a2b7628
siwuxie095 <834879583@qq.com>
2021-01-19 08:32:30 +0800

    Spring MVC 起步；

---
+++ src/main/java/com/siwuxie095/spring/chapter5th/example2nd/Main.java
@@ -0,0 +1,27 @@
+package com.siwuxie095.spring.chapter5th.example2nd;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-01-19 08:24:10
+ */
+public class Main {
+
+    /**
+     * Spring MVC 起步
+     *
+     * 你见到过孩子们的捕鼠器游戏吗？这真是一个疯狂的游戏，它的目标是发送一个小钢球，让它经过一系列稀奇古怪的装置，最后触发
+     * 捕鼠器。小钢球穿过各种复杂的配件，从一个斜坡上滚下来，被跷跷板弹起，绕过一个微型摩天轮，然后被橡胶靴从桶中踢出去。经
+     * 过这些后，小钢球会对那只可怜又无辜的橡胶老鼠进行捕获。
+     *
+     * 乍看上去，你会认为 Spring MVC 框架与捕鼠器有些类似。Spring 将请求在调度 Servlet、处理器映射（handler mapping）、
+     * 控制器以及视图解析器（view resolver）之间移动，而捕鼠器中的钢球则会在各种斜坡、跷跷板以及摩天轮之间滚动。但是，不要
+     * 将 Spring MVC 与 Rube Goldberg-esque 捕鼠器游戏做过多比较。每一个 Spring MVC 中的组件都有特定的目的，并且它也
+     * 没有那么复杂。
+     *
+     * 后续将会介绍请求是如何从客户端发起，经过 Spring MVC 中的组件，最终再返回到客户端的。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
