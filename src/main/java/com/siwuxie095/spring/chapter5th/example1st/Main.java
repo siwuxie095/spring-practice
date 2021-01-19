a1d4d2b059bd8c742142c844e5687c5077b81582
siwuxie095 <834879583@qq.com>
2021-01-19 08:16:30 +0800

    构建 Spring Web 应用程序；

---
+++ src/main/java/com/siwuxie095/spring/chapter5th/example1st/Main.java
@@ -0,0 +1,25 @@
+package com.siwuxie095.spring.chapter5th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-01-19 08:11:54
+ */
+public class Main {
+
+    /**
+     * 构建 Spring Web 应用程序
+     *
+     * 作为企业级 Java 开发者，你可能开发过一些基于 Web 的应用程序。对于很多 Java 开发人员来说，基于 Web 的应用程序是他们
+     * 主要的关注点。如果你有这方面经验的话，你会意识到这种系统所面临的挑战。具体来讲，状态管理、工作流以及验证都是需要解决的
+     * 重要特性。HTTP 协议的无状态性决定了这些问题都不那么容易解决。
+     *
+     * Spring 的 Web 框架就是为了帮你解决这些关注点而设计的。Spring MVC 基于模型-视图-控制器（Model-View-Controller，
+     * MVC）模式实现，它能够帮你构建像 Spring 框架那样灵活和松耦合的 Web 应用程序。
+     *
+     * 这里将会介绍 Spring MVC Web 框架，并使用新的 Spring MVC 注解来构建处理各种 Web 请求、参数和表单输入的控制器。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
