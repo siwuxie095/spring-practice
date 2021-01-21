3283b8065caa1344d9a03d076db4eb93cf7489ee
siwuxie095 <834879583@qq.com>
2021-01-21 21:43:18 +0800

    Spittr 应用简介；

---
+++ src/main/java/com/siwuxie095/spring/chapter5th/example5th/Main.java
@@ -0,0 +1,27 @@
+package com.siwuxie095.spring.chapter5th.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-01-21 21:38:08
+ */
+public class Main {
+
+    /**
+     * Spittr 应用简介
+     *
+     * 为了实现在线社交的功能，这里将要构建一个简单的微博（microblogging）应用。在很多方面，所构建的应用与最早的微博应用
+     * Twitter 很类似。在这个过程中，会添加一些小的变化。当然，这里要使用 Spring 技术来构建这个应用。
+     *
+     * 因为从 Twitter 借鉴了灵感并且通过 Spring 来进行实现，所以它就有了一个名字：Spitter。再进一步，应用网站命名中流行
+     * 的模式，如 Flickr，去掉字母 e，这样的话，就将这个应用称为 Spittr。这个名称也有助于区分应用名称和领域类型，因为这里
+     * 将会创建一个名为 Spitter 的领域类。
+     *
+     * Spittr 应用有两个基本的领域概念：Spitter（应用的用户）和 Spittle（用户发布的简短状态更新）。
+     *
+     * 后续会构建应用的 Web 层，创建展现 Spittle 的控制器以及处理用户注册成为 Spitter 的表单。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
