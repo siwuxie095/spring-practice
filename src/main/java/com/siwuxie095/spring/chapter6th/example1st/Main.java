94e642382b8a71eef797d076bd602bf63bfe72b3
siwuxie095 <834879583@qq.com>
2021-01-24 20:56:09 +0800

    渲染 Web 视图；

---
+++ src/main/java/com/siwuxie095/spring/chapter6th/example1st/Main.java
@@ -0,0 +1,20 @@
+package com.siwuxie095.spring.chapter6th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-01-24 20:52:30
+ */
+public class Main {
+
+    /**
+     * 渲染 Web 视图
+     *
+     * 之前主要关注于如何编写处理 Web 请求的控制器。也创建了一些简单的视图，用来渲染控制器产生的模型数据，但并
+     * 没有花太多时间讨论视图，也没有讨论控制器完成请求到结果渲染到用户的浏览器中的这段时间内到底发生了什么，这
+     * 里将着重讨论这些内容。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
