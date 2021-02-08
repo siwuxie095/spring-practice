df8425b35769d765183458f5b838556989a0e4e0
siwuxie095 <834879583@qq.com>
2021-02-08 22:44:20 +0800

    使用 Spring Web Flow；

---
+++ src/main/java/com/siwuxie095/spring/chapter8th/example1st/Main.java
@@ -0,0 +1,34 @@
+package com.siwuxie095.spring.chapter8th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-02-08 22:32:03
+ */
+public class Main {
+
+    /**
+     * 使用 Spring Web Flow
+     *
+     * 关于互联网，很奇妙的一件事就是它很容易让你迷失。有如此之多的内容可以查看和阅读，而超链接是互联网强大魔力的核心。
+     *
+     * 有时候，Web 应用程序需要控制网络冲浪者的方向，引导他们一步步地访问应用。比较典型的例子就是电子商务站点的结账
+     * 流程，从购物车开始，应用程序会引导你依次经过派送详情、账单信息以及最终的订单确认流程。
+     *
+     * Spring Web Flow 是一个 Web 框架，它适用于元素按规定流程运行的程序。在这里，将会探索 Spring Web Flow 并
+     * 了解它如何应用于 Spring Web 框架平台。
+     *
+     * 其实可以使用任何 Web 框架编写流程化的应用程序。比如，曾经看到过一个应用程序，在 Struts 中构建了特定的流程。
+     * 但是这样就没有办法将流程与实现分开了，你会发现流程的定义分散在组成流程的各个元素中。没有地方能够完整地描述整个
+     * 流程。
+     *
+     * Spring Web Flow 是 Spring MVC 的扩展，它支持开发基于流程的应用程序。它将流程的定义与实现流程行为的类和视
+     * 图分离开来。
+     *
+     * 在介绍 Spring Web Flow 的时候，将会暂时放下 Spittr 样例，并使用生成披萨订单的新 Web 应用程序。这里会使用
+     * Spring Web Flow 来定义订单流程。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
