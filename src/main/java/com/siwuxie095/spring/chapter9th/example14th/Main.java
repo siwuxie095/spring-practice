1c0dc93a224ff2b5cb3dab73d308ac37c00ebf4b
siwuxie095 <834879583@qq.com>
2021-02-20 23:09:29 +0800

    小结；

---
+++ src/main/java/com/siwuxie095/spring/chapter9th/example14th/Main.java
@@ -0,0 +1,27 @@
+package com.siwuxie095.spring.chapter9th.example14th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-02-20 23:05:06
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 对于许多应用而言，安全性都是非常重要的切面。Spring Security 提供了一种简单、灵活且强大的机制
+     * 来保护应用程序。
+     *
+     * 借助于一系列 Servlet Filter，Spring Security 能够控制对 Web 资源的访问，包括 Spring MVC
+     * 控制器。借助于 Spring Security 的 Java 配置模型，不必直接处理 Filter，能够非常简洁地声明
+     * Web 安全性功能。
+     *
+     * 当认证用户时，Spring Security 提供了多种选项。这里探讨了如何基于内存用户库、关系型数据库和
+     * LDAP 目录服务器来配置认证功能。如果这些可选方案无法满足认证需求的话，还学习了如何创建和配置
+     * 自定义的用户服务。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
