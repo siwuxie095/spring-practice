861a2a42835470b0ea2e409c56df99b67a67cb28
siwuxie095 <834879583@qq.com>
2021-02-23 22:32:42 +0800

    小结；

---
+++ src/main/java/com/siwuxie095/spring/chapter10th/example7th/Main.java
@@ -0,0 +1,26 @@
+package com.siwuxie095.spring.chapter10th.example7th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-02-23 22:29:04
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 数据是应用程序的血液。有些数据中心论者甚至主张数据即应用。鉴于数据的重要地位，以健壮、简单和清晰的方式
+     * 开发应用程序的数据访问部分就显得举足轻重了。
+     *
+     * 在 Java 中，JDBC 是与关系型数据库交互的最基本方式。但是按照规范，JDBC 有些太笨重了。Spring 能够解除
+     * 使用 JDBC 中的大多数痛苦，包括消除样板式代码、简化 JDBC 异常处理，你所需要做的仅仅是关注要执行的 SQL
+     * 语句。
+     *
+     * 在这里，学习了 Spring 对数据持久化的支持，以及 Spring 为 JDBC 所提供的基于模板的抽象，它能够极大地
+     * 简化 JDBC 的使用。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
