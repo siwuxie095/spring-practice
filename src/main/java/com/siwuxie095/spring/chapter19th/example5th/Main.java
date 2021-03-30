4655bdd4ac677ef6e08d0319ddbd063029855607
siwuxie095 <834879583@qq.com>
2021-03-30 22:51:00 +0800

    小结；

---
+++ src/main/java/com/siwuxie095/spring/chapter19th/example5th/Main.java
@@ -0,0 +1,23 @@
+package com.siwuxie095.spring.chapter19th.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-03-30 22:47:29
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * Email 是人与人之间通信的重要形式，通常也是应用与人进行通信的一种形式。Spring 基于 Java 所提供的 Email
+     * 功能，抽象了 JavaMail，使得在 Spring 中使用和配置起来都更加简单。
+     *
+     * 在这里，看到了如何使用 Spring 的 Email 抽象功能发送简单的 Email 消息，然后更进一步，学习了如何发送包含
+     * 附件和经过 HTML 格式化的富文本消息。还看到了如何使用像 Velocity 和 Thymeleaf 这样的模板引擎生成富文本
+     * Email 文本，避免了通过字符串拼接创建 HTML。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
