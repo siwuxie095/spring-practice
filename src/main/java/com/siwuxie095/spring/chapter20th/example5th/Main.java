461a644cf7bc50d37102f17c9fc38639d8449bf2
siwuxie095 <834879583@qq.com>
2021-04-01 22:21:27 +0800

    小结；

---
+++ src/main/java/com/siwuxie095/spring/chapter20th/example5th/Main.java
@@ -0,0 +1,20 @@
+package com.siwuxie095.spring.chapter20th.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-04-01 22:19:45
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * JMX 是对应用程序进行操纵的一扇窗口。在这里，了解了如何配置 Spring 自动地把 Spring bean 导出为
+     * JMX MBean，从而可以让开发者通过 JMX 管理工具查看和操作 bean 的信息。也了解了当 MBean 和工具彼
+     * 此距离很远时，如何创建和使用远程 MBean。最后，还了解了如何使用 Spring 发布和监听 JMX 通知。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
